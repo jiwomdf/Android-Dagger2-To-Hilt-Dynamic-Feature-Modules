57bd37cf3e17b2c5b180357b1f004e1f8f2a2fe5
jiwomdf <mdf.jiwo@gmail.com>
2022-06-02 21:35:18 +0700

    add fragment animation

--- app/src/androidTest/java/com/example/capstone/feature/main/HomeFragmentTest.kt
+++ app/src/androidTest/java/com/example/capstone/feature/main/HomeFragmentTest.kt
@@ -1,3 +1,19 @@
 package com.example.capstone.feature.main
 
-class HomeFragmentTest
+import androidx.core.os.bundleOf
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class HomeFragmentTest {
+
+    @Test
+    fun testEventFragment() {
+        // The "fragmentArgs" argument is optional.
+        val fragmentArgs = bundleOf()
+        val scenario = launchFragmentInContainer<HomeFragment>(fragmentArgs)
+    }
+
+}
